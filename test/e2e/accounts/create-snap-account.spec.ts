90b38f25c4e6e35a91824094d221d69dd0c1d470
david0xd <david@timechaser.org>
2024-07-11 12:25:25 +0200

    Revert e2e test changes for accounts

--- test/e2e/accounts/create-snap-account.spec.ts
+++ test/e2e/accounts/create-snap-account.spec.ts
@@ -42,17 +42,6 @@ describe('Create Snap Account', function (this: Suite) {
           text: 'Confirm',
           tag: 'button',
         });
-
-        // wait for permissions popover, click checkboxes and confirm
-        await driver.waitForSelector('.mm-checkbox__input');
-        await driver.clickElement('.mm-checkbox__input');
-        await driver.waitForSelector(
-          '[data-testid="snap-install-warning-modal-confirm"]',
-        );
-        await driver.clickElement(
-          '[data-testid="snap-install-warning-modal-confirm"]',
-        );
-
         await driver.waitForSelector({ text: 'OK' });
         await driver.clickElement({
           text: 'OK',
