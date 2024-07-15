f78a0cbed93c2a1bbe8b5d705d82e2a0f0830759
Pedro Figueiredo <pedro.figueiredo@consensys.net>
2024-07-15 11:37:27 +0100

    wip

--- ui/pages/confirmations/token-allowance/token-allowance.js
+++ ui/pages/confirmations/token-allowance/token-allowance.js
@@ -72,6 +72,7 @@ import { ConfirmPageContainerWarning } from '../components/confirm-page-containe
 import CustomNonce from '../components/custom-nonce';
 import FeeDetailsComponent from '../components/fee-details-component/fee-details-component';
 import { BlockaidResultType } from '../../../../shared/constants/security-provider';
+import { ExistingRequestsBannerAlert } from '../confirmation/components/existing-requests-banner-alert/existing-requests-banner-alert';
 
 const ALLOWED_HOSTS = ['portfolio.metamask.io'];
 
@@ -384,6 +385,7 @@ export default function TokenAllowance({
         marginLeft={4}
         marginRight={4}
       />
+      <ExistingRequestsBannerAlert />
       {isSuspiciousResponse(txData?.securityProviderResponse) && (
         <SecurityProviderBannerMessage
           securityProviderResponse={txData.securityProviderResponse}
